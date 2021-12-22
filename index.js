f9f86f8318ef14d1a2b1c955a1540bcd1bacd747
PatiSal <76459273+PatiSal@users.noreply.github.com>
2021-12-22 23:46:42 +0000

    coiso

--- index.js
+++ index.js
@@ -12,25 +12,27 @@ menuBtn.addEventListener('click', () => {
 });
 
 
-function castParallax() {
+let bg= document.getElementById("bg");
+let pinheiro = document.getElementById("pinheiro");
+let monte = document.getElementById("monte");
+let arvore1 = document.getElementById("arvore1");
+let arvore2 = document.getElementById("arvore2");
+let frentepinheiro = document.getElementById("frentepinheiro");
+let prado = document.getElementById("prado");
+let veado = document.getElementById("veado");
+let arbusto = document.getElementById("arbusto");
 
-	var opThresh = 350;
-	var opFactor = 750;
+window.addEventListener ('scroll', function(){
+	var value = window.scrollY;
 
-	window.addEventListener("scroll", function(event){
-
-		var top = this.window.pageYOffset;
-		var layers = document.getElementsByClassName("parallax");
-		var layer, speed, yPos;
-		
-		for (var i = 0; i < layers.length; i++) {
-			layer = layers[i];
-			speed = layer.getAttribute('data-speed');
-			var yPos = -(top * speed / 100);
-			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 5px)');
-
-		}
-	});
-}
-
-document.body.onload = castParallax();
+	bg.style.top = value * 0.5 + 'px';
+	pinheiro.style.top = -value * 0.5 +'px';
+	monte.style.top = -value *0.5 +'px';
+	arvore1.style.top = -value * 0.15 + 'px';
+	arvore2.style.top = -value * 0.15 + 'px';
+	frentepinheiro.style.top = value * 0.15 + 'px';
+	prado.style.top = value * 0.15 + 'px';
+	veado.style.top = value * 0.15 + 'px';
+	arbusto.style.top = value * 0.05 + 'px';
+})
+	
