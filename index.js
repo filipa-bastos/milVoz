2ef330efa75964337b447628f1a8ff74f3c4f06f
PatiSal <76459273+PatiSal@users.noreply.github.com>
2021-12-26 16:11:06 +0000

    naosei

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// IMPORTAR ANIMAÇÕES 
-import * as mdb from 'mdb-ui-kit'; // lib
-import { Input } from 'mdb-ui-kit'; // module
-import { Input as CustomInput } from 'mdb-ui-kit'; // module with custom name
 
 
 
@@ -43,4 +39,36 @@ window.addEventListener ('scroll', function(){
 	veado.style.top = value * 0.15 + 'px';
 	arbusto.style.top = value * 0.05 + 'px';
 })
+
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+
+var btn1 = document.getElementById("alegria");
+var btn2 = document.getElementById("pescaria");
+
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 	
+btn1.onclick = function() {
+	modal1.style.display = "block";
+  }
+  
+  btn2.onclick = function() {
+	modal2.style.display = "block";
+  }
+
+  span1.onclick = function() {
+	modal1.style.display = "none";
+  }
+  
+  span2.onclick = function() {
+	modal2.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+	if (event.target == modal1 ) {
+	  modal1.style.display = "none";
+	}else if (event.target == modal2){
+	  modal2.style.display = "none";
+	}
+  }
