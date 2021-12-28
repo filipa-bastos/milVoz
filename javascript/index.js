57644405a98784cada788b70a6c8f2e11a6db150
PatiSal <76459273+PatiSal@users.noreply.github.com>
2021-12-28 20:07:53 +0000

    resolvido

--- javascript/index.js
+++ javascript/index.js
@@ -73,3 +73,35 @@ btn1.onmouseover = function() {
 	  modal2.style.display = "none";
 	}
   }
+
+
+let span = document.getElementsByTagName('span');
+let mamiferos = document.getElementsByClassName('mamifero');
+let l = 0;
+//movimento em função da largura dos cartoes
+let movimento = mamiferos[0].offsetWidth ;
+
+
+//============MAMIFEROS 
+//função para mover para a direita
+function moveDirMamiferos(){
+    l = l + movimento;
+    for(const m of mamiferos){
+       //limite do ultimo item mostrado em função da seta (span à direita)
+        if(mamiferos[mamiferos.length-1].offsetLeft + mamiferos[mamiferos.length-1].offsetWidth > span[2].offsetLeft){
+            m.style.left = '-' + l + 'px';
+        }        
+    }
+}
+
+function moveEsqMamiferos(){
+    l = l - movimento;
+    for(const m of mamiferos){
+            m.style.left = '-' + l + 'px';        
+    }
+}
+
+
+
+span[2].onclick  = ()=>{moveEsqMamiferos();}
+span[3].onclick  = ()=>{moveDirMamiferos();}
