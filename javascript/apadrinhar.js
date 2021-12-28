6e8abc91f1d2dfb32f25018516255e98b3f1920d
Filipa Bastos <87571884+filipa-bastos@users.noreply.github.com>
2021-12-28 17:36:22 +0000

    done

--- javascript/apadrinhar.js
+++ javascript/apadrinhar.js
@@ -10,21 +10,14 @@ for(var j = 0; j < animais.length; j++) {
 }
 
 
-
 //MODAL
 // Get the modal
 var formApadrinharDisplay = document.getElementById("formApadrinharDisplay");
 
-// Get the button that opens the modal - BOTAO avaliar de necessario
-// var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal   - BOTAO avaliar de necessario
-//btn.onclick = function() {
- // modal.style.display = "block";
-//}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
