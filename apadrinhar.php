9fba2f342a85e177df37328b6b8888da384dc03d
Filipa Bastos <87571884+filipa-bastos@users.noreply.github.com>
2022-01-28 22:48:34 +0000

    Update apadrinhar.php

--- apadrinhar.php
+++ apadrinhar.php
@@ -189,7 +189,7 @@
                     </dl>
                     <br>
                     <dl>
-                    <a href="#">
+                    <a href="jogo.html">
                         <dt> <h2>JOGO</h2></dt>
                     </a>
                     </dl>
